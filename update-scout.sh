86c0dd670d0e0248621e787219998b4ba8b694a5
Paul Norton <pnorton@duckblade.com>
2023-09-25 11:51:29 -0400

    update scout-cli

--- update-scout.sh
+++ update-scout.sh
@@ -2,12 +2,13 @@
 set -euxo pipefail
 
 pushd ~/IdeaProjects/scout-cli-plugin
-	task go:build:action
+	go-task go:build:action
 popd
 
 cp ../scout-cli-plugin/dist/github-action ./dist/docker-scout-action_linux_amd64
 git add ./dist/docker-scout-action_linux_amd64
 git commit -m "update scout-cli" 
 git tag -s -f -m testing testing HEAD 
-git push --follow-tags
+git push
+git push --tags --force
 
